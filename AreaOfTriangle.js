1eca35329242ecd9727dbf253449221e16f685cd
Carol Nyiva <45183823+Carol-Nyiva@users.noreply.github.com>
2020-06-11 21:24:47 +0300

    Add files via upload

---
+++ AreaOfTriangle.js
@@ -0,0 +1,6 @@
+//Write a function that takes the base and height of a triangle and return its area
+
+
+
+const CalcArea = (base,height) => (base *height)/2;
+console.log(CalcArea(40,12));
---
+++ FrontEndMastersExample.js
@@ -0,0 +1,24 @@
+var foo = "foo";
+
+function bob()
+{
+    var foo = "foo2";
+    console.log(foo);    //foo2
+}
+
+bob();
+console.log(foo);        //foo
+
+
+//....................................................//
+
+var foo = "foo";
+
+function bob()
+{
+    var foo = "foo2";
+    console.log(foo);    //foo2
+}
+
+( bob )();
+console.log(foo);    //foo
